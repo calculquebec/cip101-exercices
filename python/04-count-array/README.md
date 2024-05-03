fb86f9da59c4d5f07ed88c9c71759f3285adf579
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-05-02 20:28:50 -0400

    Added an example of data parallelism, with job arrays

---
+++ python/04-count-array/README.md
@@ -0,0 +1,9 @@
+# Exercice 04
+
+Soumettre une tâche qui compte le nombre de mots dans les fichiers donnés.
+Vous devez charger le module Python.
+
+---
+
+Submit a task thats count number of words in each given file.
+You must load a Python module.
---
+++ python/04-count-array/submit.sh.sol
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --account=def-sponsor00
+#SBATCH --time=00:01:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=512M
+#SBATCH --array=1-4
+
+module load python/3.11
+
+# Select a file according to which sub-task id
+echo "Starting task $SLURM_ARRAY_TASK_ID"
+filename=$(sed -n "${SLURM_ARRAY_TASK_ID}p" files-list.txt)
+
+# Will count words in one file.
+echo "Processing file : $filename"
+python seq-count.py $filename > results.$SLURM_ARRAY_TASK_ID
+
+# One could then aggregate the results with:
+# cat results.* > all-results
