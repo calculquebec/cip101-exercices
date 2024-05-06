e1b209bc83c365244f0e407615e48881fd212a68
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-05-06 16:23:21 -0400

    fix mpi hw

--- python/05-mpi/submit-mpi.sh.sol
+++ python/05-mpi/submit-mpi.sh.sol
@@ -2,7 +2,7 @@
 #SBATCH --account=def-sponsor00
 #SBATCH --time=00:30:00
 #SBATCH --ntasks=4
-#SBATCH --mem-per-cpu=3000M
+#SBATCH --mem=4000M
 
 # Will run ntask accross available cores on nodes (same or different)
 # Specify
@@ -10,7 +10,7 @@
 # #SBATCH --nodes=2
 # to run 4 tasks accross two nodes.
 
-module load StdEnv/2023 python/3.11 mpi4py
+module load StdEnv/2023 gcc python/3.11 openmpi mpi4py/3.1.4
 
 # srun exports the current env, which contains $VIRTUAL_ENV and $PATH variables
 srun python hw-mpi.py
